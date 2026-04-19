7ec5dfa42f9aeaff0a8d01c89bb9eea4055f1baa
LDeathKira <lucafabrianilavoro@gmail.com>
2026-04-19 12:22:30 +0200

    nuova esercitazione

---
+++ Esercizi Variabili, tipi di dati e Casting.py
@@ -0,0 +1,95 @@
+es1 = "Esercizio 1"
+print (es1)
+
+name = "Luca"
+age = "27"
+city = "Toscana"
+print (name, age, city)
+
+
+es2 = "Esercizio 2"
+print (es2)
+
+x = 5
+print (x)
+x = 10 
+print (x)
+
+
+es3 = "Esercizio 3"
+print (es3)
+
+a = 10
+b = 20
+somma = (a + b)
+print (somma)
+
+
+es4 = "Esercizio 4"
+print (es4)
+
+c = 20
+d = 15
+c,d = d,c
+print (c,d)
+
+
+es5 = "Esercizio 5"
+print (es5)
+
+base = 4
+altezza = 2
+area = base * altezza
+print ("tenendo conto dei valori base 4 e altezza 2, l'area sarà:", area)
+
+
+es6 = "Esercizio 6"
+print (es6)
+
+intero = 10
+decimale = 2.5
+print (intero + decimale)
+
+
+es7 = "Esercizio 7"
+print (es7)
+
+valore1 = 10
+valore2 = 10
+valore3 = 9
+media = (valore1 + valore2 + valore3) / 3
+print ("Dati 3 valori: 10-10-9, la media è", media) 
+
+
+es8 = "Esercizio 8"
+print (es8)
+
+s1 = "tra poco "
+s2 = "andrò a giocare "
+s3 = "a Dark Souls"
+frase = s1 + s2 + s3
+print (frase)
+
+
+es9 = "Esercizio 9"
+print (es9)
+
+ripetere = "Muda! "
+print (ripetere * 3)
+
+
+es10 = "Esercizio 10"
+print (es10)
+
+n1 = 10
+n2 = 15
+n3 = 5
+print (n1 < n2, n1 > n3, n3 > n2)
+
+
+es11 = "Esercizio 11"
+print (es11)
+
+numero = 6
+print (float(numero))
+print ("Il numero diventa stringa:", str(numero))
